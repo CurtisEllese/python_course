a866257c222e559c293dbd048ebc194d92a17e94
CurtisEllese <mama-karina@bk.ru>
2023-03-18 02:06:29 +0600

    Solved Task 12

--- task12.py
+++ task12.py
@@ -2,3 +2,15 @@
 # 4 4 -> 2 2 
 # 5 6 -> 2 3 
 
+sum_of_numbers = int(input("Введите сумму чисел: "))
+product_of_numbers = int(input("Введите произведение чисел: "))
+
+num1 = 1
+num2 = 1
+while sum_of_numbers != num1 + num2 and product_of_numbers != num1 * num2:
+    if product_of_numbers != (sum_of_numbers - num2) * num2:
+        num2 += 1
+    if num1 != sum_of_numbers - num2:
+        num1 += 1
+
+print(num1, num2)
