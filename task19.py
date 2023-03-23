44b4d1d45daaaa217b05e756e4c9c321bda428f7
CurtisEllese <mama-karina@bk.ru>
2023-03-24 01:51:15 +0600

    Add new solution in Task 19

--- task19.py
+++ task19.py
@@ -5,15 +5,24 @@
 
 list_1 = [1, 2, 3, 4, 5]
 k = int(input("Введите число сдвига: "))
-while k > 0:
-    list_1.append(list_1.pop(0))
-    k -= 1
+for i in range(k % len(list_1)):
+    list_1.insert(0, list_1.pop(-1))
 print(list_1)
 
-# list_1 = [2,4,6,8,10]
+# list_1 = [1, 2, 3, 4, 5]
 # k = int(input("Введите число сдвига: "))
+# while k > 0:
+#     list_1.insert(0, list_1.pop(-1))
+#     k -= 1
+# print(list_1)
 
-# print(list_1[k:] + list_1[:k])
+# list_1 = [1, 2, 3, 4, 5]
+# k = int(input("Введите число сдвига: "))
+# list_2 = list()
+# for i in range(len(list_1)):
+#     list_2.insert(i, list_1[i - k])
+
+# print(list_2)
 
 # list_1 = [1, 2, 3, 4, 5]
 # k = int(input("Введите число сдвига: "))
